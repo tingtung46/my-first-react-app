372d9a0bb82da31ff08f947c3ffc540104fefc1e
root <132725436+tingtung46@users.noreply.github.com>
2024-08-18 12:11:51 +0700

    Update App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+function Button({
+  text = "Click Me!",
+  color = "blue",
+  fontSize = 12,
+  handleClick,
+}) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello, World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button
+      onClick={() => handleClick("https://www.theodinproject.com")}
+      style={buttonStyle}
+    >
+      {text}
+    </button>
   );
 }
 
-export default App;
+export default function App() {
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
+
+  return (
+    <div>
+      <Button handleClick={handleButtonClick} />
+    </div>
+  );
+}
