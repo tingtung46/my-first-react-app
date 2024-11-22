ce17dfecf5a5db2c6eede38b7e177ffaddc8dff2
root <132725436+tingtung46@users.noreply.github.com>
2024-11-22 09:43:19 +0700

    Set up react testing environment

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Person from "./Person.jsx";
 import "./index.css";
+import Clock from "./Clock.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Person />
+    <Clock />
   </StrictMode>
 );
